874ce382b1ab6385e9bc0abc8979c873c0b7cabf
Jireh <Jirehreed333@gmail.com>
2021-03-12 21:02:20 -0500

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Jireh  Reed on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
